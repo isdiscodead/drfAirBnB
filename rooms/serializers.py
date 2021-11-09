92a2c3181f93197f42037c5ceb030289f532663a
isdiscodead <isdiscodead@korea.ac.kr>
2021-11-09 23:05:20 +0900

    #1.3

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
+from rooms.models import Room
+from users.serializers import TinyUserSerializer
 
-class RoomSerializer(serializers.Serializer):
+
+class RoomSerializer(serializers.ModelSerializer):
     # Room model에 있는 field 가져오기
-    name = serializers.CharField(max_length=140)
-    price = serializers.IntegerField()
-    bedrooms = serializers.IntegerField()
-    instant_book = serializers.BooleanField()
+
+    # name = serializers.CharField(max_length=140)
+    # price = serializers.IntegerField()
+    # bedrooms = serializers.IntegerField()
+    # instant_book = serializers.BooleanField()
+
+    # user 객체의 데이터를 보기 좋게, 따로 Serialize !
+    user = TinyUserSerializer()
+
+    class Meta:
+        model = Room
+        fields = ("name", "price", "instant_book", "bedrooms", "user")
