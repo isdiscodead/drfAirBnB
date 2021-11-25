4c54476a443fc648ab900462704d604b0396202e
isdiscodead <isdiscodead@korea.ac.kr>
2021-11-25 09:35:46 +0900

    add favs on viewsetF

--- users/views.py
+++ users/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth import authenticate
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes, action
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from rooms.models import Room
@@ -23,11 +22,10 @@ class UserViewSet(ModelViewSet):
         permission_classes = []
         if self.action == "list":
             permission_classes = [IsAdminUser]
-        elif self.action == "create" or self.action == "retrieve":
+        elif self.action == "create" or self.action == "retrieve" or self.action == "favs":
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsSelf]
-
         return [permission() for permission in permission_classes]
 
     @action(detail=False, methods="POST")
@@ -45,31 +43,16 @@ class UserViewSet(ModelViewSet):
             else:
                 return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(UserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-# 두 가지 이상의 접근 방식을 사용해야 하는 경우 class view가 더 적합함!
-class FavsView(APIView):
-
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
+    @action(detail=True)
+    def favs(self, request, pk):
         user = request.user
         serializer = RoomSerializer(user.favs.all(), many=True)
         return Response(serializer.data)
 
-    # 이런 방식의 DB 수정을 put으로 할지 post로 할지는 선택임
-    def put(self, request):
+    @favs.mapping.put
+    def toggle_favs(self, request, pk):
         pk = request.data.get("pk", None)
         user = request.user
-
         if pk is not None:
             try:
                 room = Room.objects.get(pk=pk)
@@ -83,3 +66,12 @@ class FavsView(APIView):
                 pass
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
